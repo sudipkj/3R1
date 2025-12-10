c034dfaf0a884b94fe4aa42fc85c6d3cabaf159f
Sudipkj <sudipkj@gmail.com>
2025-12-10 21:16:44 +0530

    Class component

---
+++ intro-app/src/ClassComponent.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class MyClass extends Component{
+
+    constructor(){
+        super();
+    }
+
+    render(){
+
+        return <div>
+            <p>This is para</p>
+        </div>
+    }
+}
+
+export default MyClass;
